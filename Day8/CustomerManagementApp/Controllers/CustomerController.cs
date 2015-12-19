24c81104c96ef622907fc695b5bc8b733133500f
deniz@armut.com <deniz@armut.com>
2015-12-19 15:24:37 +0200

    Customer örnek uygulaması için MVC uygulaması oluşturuldu.

---
+++ Day8/CustomerManagementApp/Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomerManagementApp.Models;
+
+namespace CustomerManagementApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            IList<CustomerModel> customerModels = new List<CustomerModel>
+            {
+                new CustomerModel()
+                {
+                    Id = 1,
+                    FirstName = "Deniz",
+                    LastName = "İrgin",
+                    EmailAddress = "deniz@armut.com",
+                    HomeAddress = "Kartal",
+                    WorkAddress = "Kadıköy"
+                },
+                new CustomerModel()
+                {
+                    Id = 2,
+                    FirstName = "Hüseyin",
+                    LastName = "Yazıcı",
+                    EmailAddress = "huseyin@ponera.com",
+                    HomeAddress = "Ataşehir",
+                    WorkAddress = "Kozyatağı"
+                },
+            };
+
+            // return Content("dasfasdasdas");
+            // return Json()
+
+            return View(customerModels);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CustomerModel customerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Day8/CustomerManagementApp/Models/CustomerModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerManagementApp.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Lütfen doğru Email adresi giriniz")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string HomeAddress { get; set; }
+
+        [Required]
+        public string WorkAddress { get; set; }
+    }
+}
