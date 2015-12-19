0153fd6f2370307201ee6e4280709f3927491be0
deniz@armut.com <deniz@armut.com>
2015-12-19 18:44:04 +0200

    Javascript module pattern örneği ve bundle'a jquery validation eklenmesi.

---
+++ Day8/CustomerManagementApp/Scripts/View/Customer/customer.js
@@ -0,0 +1,19 @@
+﻿window.Ponera = {};
+
+window.Ponera.Customer = (function(window, $) {
+    "use strict";
+
+    $(document).ready(function() {
+        $('.btn').click(function() {
+            var valid = $('form').valid();
+
+            if (valid) {
+                $('form').submit();
+            }
+        });
+    });
+
+    return {
+
+    };
+}(window, $));
---
+++ Day8/CustomerManagementApp/Scripts/View/Customer/example.js
@@ -0,0 +1,20 @@
+﻿window.Ponera = {};
+
+window.Ponera.Customer = (function() {
+    "use strict";
+
+    var number = 0;
+    console.log(number);
+
+    return {
+        benBirPublicMethodum: function() {
+            console.log("Wuhuuuu");
+        },
+        getNumber : function() {
+            return number;
+        },
+        setNumber : function(input) {
+            number = input;
+        }
+    };
+}());
