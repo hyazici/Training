0dfef6374284999f9866f11c805f45b27a76f9a4
deniz@armut.com <deniz@armut.com>
2015-12-26 16:41:44 +0200

    solution fix

---
+++ Day9/PartialClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvoiceManager invoiceManager = new InvoiceManager();
+            var amount = invoiceManager.Amount;
+        }
+    }
+
+    public partial class InvoiceManager
+    {
+        public void SetInvoice()
+        {
+        }
+
+        public void CancelInvoice()
+        {            
+        }
+    }
+
+    public partial class InvoiceManager
+    {
+        public double Amount { get; set; }
+    }
+}
