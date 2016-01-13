7784fa66aca43152f8001e3ecb52023d20700a2c
hyazici <yazicihuseyin@gmail.com>
2016-01-13 20:41:27 +0200

    Generic part 1

---
+++ Day10/GenericRepository/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public interface IRepository<TEntity, KPrimaryKey>
+    {
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        TEntity Get(KPrimaryKey primaryKey);
+
+        void Delete(TEntity entity);
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class CustomerRepository : IRepository<Customer, int>
+    {
+        public void Add(Customer entity)
+        {
+            // git dapper'ı çağır
+            // query'ni yaz
+            // dapper'ı dispose et
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int primaryKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //public abstract class BaseRepository<T>
+    //{
+    //    pub
+    //}
+}
---
+++ Day10/Generics/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myList = new ArrayList();
+
+            // myList.Add()
+
+            MyStack<int> intList = new MyStack<int>();
+            MyStack<string> strList = new MyStack<string>();
+            MyStack<Customer> cusList = new MyStack<Customer>();
+
+            // Olmaz çünkü int bir class değil
+            // MyGenericClassCons<int> genericClas = new MyGenericClassCons<int>();
+
+            //MyGenericStructCons<Customer> genericCust = new MyGenericStructCons<Customer>
+            MyGenericStructCons<int> genericCust = new MyGenericStructCons<int>();
+            MyGenericClassCons2<Order> order = new MyGenericClassCons2<Order>();
+
+            MyGenericClass<Circle> Circle = new MyGenericClass<Circle>();
+
+            Circle.Foo();
+        }
+    }
+
+    class MyGenericClassCons<T> 
+        where T : class
+    {
+
+    }
+
+    class MyGenericStructCons<T>
+        where T : struct
+    {
+
+    }
+
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+
+        }
+    }
+
+    class MyGenericClassCons2<T>
+        where T : class, 
+        new()
+    {
+
+    }
+
+    class MyGenericClass<T>
+        where T : IShape,
+        new ()
+    {
+        public void Foo()
+        {
+            T shape = new T();
+
+            double result = shape.CalcArea();
+
+            Console.WriteLine(result);
+        }
+    }
+
+    public interface IShape
+    {
+        double CalcArea();
+    }
+
+    public class Circle : IShape
+    {
+        public double CalcArea()
+        {
+            return 100;
+        }
+    }
+
+    public class Square : IShape
+    {
+        public double CalcArea()
+        {
+            return 200;
+        }
+    }
+
+    class MyGenericType<T, K>
+        where T : class, new()
+        where K : struct
+    {
+    }
+}
