32875868086dc45d6fbbbe434ef531c059e7c3a7
unal kirikci <unalkirikci@gmail.com>
2016-02-14 15:46:10 +0200

    displayname added

--- Metronic/Base/Models/Models/CountryModel.cs
+++ Metronic/Base/Models/Models/CountryModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Ponera.Base.Models
     {
         public int Id { get; set; }
 
+        [DisplayName("İl Adı")]
         public string CountryName { get; set; }
     }
 }
