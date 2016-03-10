d754838368fbe6c0763527512eb152a99ee5e332
unal k <unalkirikci@gmail.com>
2016-03-10 14:28:47 +0200

    attributes added

---
+++ Metronic/Base/BusinessLayer/Attributes/UnitOfWorkAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponera.Base.BusinessLayer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class UnitOfWorkAttribute : Attribute
+    {
+        public UnitOfWorkAttribute(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+        {
+            IsolationLevel = isolationLevel;
+        }
+
+        public IsolationLevel IsolationLevel { get; }
+    }
+}
