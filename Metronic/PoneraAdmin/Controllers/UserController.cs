4ab908cd6bed64792c13b48001308972d4ca98ab
unal k <unalkirikci@gmail.com>
2016-03-16 15:41:50 +0200

    User page added

---
+++ Metronic/PoneraAdmin/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ponera.PoneraAdmin.Core;
+
+namespace PoneraAdmin.Controllers
+{
+    public class UserController : SecureBaseController
+    {
+        public UserController()
+        {
+                
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            //IList<UserModel> _users = _securityBusiness.Get();
+
+            return View();
+        }
+    }
+}
